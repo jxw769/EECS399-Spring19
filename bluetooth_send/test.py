f863d1f4916e103a0996e5dec7fb089369c47374
phantoriahx <hxl576@case.edu>
2019-02-28 20:38:42 +0000

    setting repository ready for image reading.

--- bluetooth_send/test.py
+++ bluetooth_send/test.py
@@ -6,15 +6,23 @@ port = 1
 server_sock.bind(("",port))
 server_sock.listen(1)
 
+#confirm connection
 client_sock, address = server_sock.accept()
 print ("Accepted connection from", address)
 
 try:
     while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print ("received [%s]" % data)
-        client_sock.send("hello!")
+        #receive data
+#       received_data = json.loads(client_sock.recv(1024)) #Terminal use bytes not json
+        received_data = client_sock.recv(1024)
+        if len(received_data) == 0: break
+        print ("received [%s]" % received_data)
+
+        #send data
+        filedata = '1' #read img data here
+        msg = json.dumps({"data": filedata, "cmd":{"transmit_flag":True}}) #add cmd here
+        client_sock.send(msg)
+
 except IOError:
     pass
 
